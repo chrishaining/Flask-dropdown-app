77c9d6a2782d4b44856ede4d6828575c67d94823
Chris <chrishaining@yahoo.co.uk>
2020-02-28 15:30:01 +0000

    gets rid of the db elements, as they are not essential to this app

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,9 @@
-from app import app, db
-# from app.models import Player 
+from app import app
 from flask import render_template, redirect, request
-# from sqlalchemy import update
 
-# @app.route('/')
-# def index():
-#     colours = []
-#     return render_template('index.html', title="Home", colours=colours)
 
 @app.route('/', methods=['GET', 'POST'])
 def colours():
-    # colours = ['Red', 'Blue', 'Yellow']
     colours = []
     colour = request.form.get('colour')
     colours.append(colour)
